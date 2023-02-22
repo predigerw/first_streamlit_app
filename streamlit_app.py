a64d69185e969a36c1c8cfebbd601eef92f281da
predigerw <87037558+predigerw@users.noreply.github.com>
2023-02-22 16:03:52 -0300

    Update streamlit_app.py
    agregados header y 3 opciones.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+stremlit.header("Breakfast Menu")
+
+streamlit.text("Omega 3 and Blueberry Meal")
+
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+
+streamlit.text("Hard-Boiled Free-Range Egg")
