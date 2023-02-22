395cab2114ba8d22758e6925f8bf318b1b0691ae
predigerw <87037558+predigerw@users.noreply.github.com>
2023-02-22 16:33:25 -0300

    Update streamlit_app.py
    agregado de iconos

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("Omega 3 and Blueberry Meal")
+streamlit.text("🥣 Omega 3 and Blueberry Meal")
 
-streamlit.text("Kale, Spinach and Rocket Smoothie")
+streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
 
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+
+streamlit.text("🥑 🍞 Avocado Toast")
