ecd167b235c58959194f549945c6f4c5d7b815f1
predigerw <87037558+predigerw@users.noreply.github.com>
2023-02-22 19:22:53 -0300

    agragando pandas
    agregado de pandas para ver un dataframe de frutas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -13,3 +14,8 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑 🍞 Avocado Toast")
 
 streamlit.header("🍌🥭 Build your own fruit smoothie 🥝🍇")
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
