462aac6d21ca5c1d83dfa7fd23bae3c3d58f99bf
predigerw <87037558+predigerw@users.noreply.github.com>
2023-02-24 10:20:04 -0300

    user selection added

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,13 @@ streamlit.dataframe(fruits_to_show)
 #new section to display the fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
+streamlit.write("The user entered" , fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())  we don't need this anymore
 
 #now we normalize the view
